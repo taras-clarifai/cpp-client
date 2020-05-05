6a15fa92eb9728e49b185b57badd65c91e0e79e6
Taras Kushnir <taras.kushnir@clarifai.com>
2020-05-05 18:58:25 +0300

    Use Go 1.14.1

--- scripts/ci/actions-install-linux.sh
+++ scripts/ci/actions-install-linux.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y --no-install-recommends build-essential autoconf libtool
 sudo apt-get install -y --no-install-recommends gdb ccache curl libcurl4-openssl-dev lcov
 
 # Golang (for gRPC)
-wget https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz
-sudo tar -xvf go1.14.2.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz
+sudo tar -xvf go1.14.1.linux-amd64.tar.gz
 sudo mv go /usr/local
 export GOROOT=/usr/local/go
