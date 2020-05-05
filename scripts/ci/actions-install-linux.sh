b5dbdb52ab58bfa530ac10d8377e318f6f782ddc
Taras Kushnir <taras.kushnir@clarifai.com>
2020-05-05 18:53:13 +0300

    Install more CI packages

--- scripts/ci/actions-install-linux.sh
+++ scripts/ci/actions-install-linux.sh
@@ -3,7 +3,8 @@
 set -x
 
 sudo apt-get update -qq
-sudo apt-get install -y --no-install-recommends build-essential gdb ccache curl libcurl4-openssl-dev lcov
+sudo apt-get install -y --no-install-recommends build-essential autoconf libtool pkg-config cmake
+sudo apt-get install -y --no-install-recommends gdb ccache curl libcurl4-openssl-dev lcov
 
 # Golang (for gRPC)
 wget https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz
