fe653ce4b70dc6375d3db46c52cd1a2272955928
Taras Kushnir <taras.kushnir@clarifai.com>
2020-05-05 18:39:40 +0300

    Install golang on github action

--- scripts/ci/actions-install-linux.sh
+++ scripts/ci/actions-install-linux.sh
@@ -4,3 +4,9 @@ set -x
 
 sudo apt-get update -qq
 sudo apt-get install -y --no-install-recommends build-essential gdb ccache curl libcurl4-openssl-dev lcov
+
+# Golang (for gRPC)
+wget https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz
+sudo tar -xvf go1.13.3.linux-amd64.tar.gz
+sudo mv go /usr/local
+export GOROOT=/usr/local/go
