6ffb33f1f28c5aa28f529b8635fd66e83920103b
Taras Kushnir <taras.kushnir@clarifai.com>
2020-05-05 11:55:19 +0300

    Install lcov

--- scripts/ci/actions-install-linux.sh
+++ scripts/ci/actions-install-linux.sh
@@ -3,4 +3,4 @@
 set -x
 
 sudo apt-get update -qq
-sudo apt-get install -y --no-install-recommends build-essential gdb ccache curl libcurl4-openssl-dev
+sudo apt-get install -y --no-install-recommends build-essential gdb ccache curl libcurl4-openssl-dev lcov
